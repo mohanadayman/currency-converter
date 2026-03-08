4997a654ec3f8a56c515558679afe7c04870ecd1
Mohanad <mohanad5ayman@gmail.com>
2026-03-08 02:05:51 +0200

    fix error

--- src/components/AmountInput.jsx
+++ src/components/AmountInput.jsx
@@ -5,7 +5,7 @@ export default function AmountInput({ value, onChange, disabled = false }) {
     <input
       type="number"
       step="any"
-      className={`w-full h-10 bg-gray-100 border border-gray-300 px-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 flex-1 rounded-l-lg bg-gray-200 border-0`}
+      className={`w-full h-10 bg-gray-100 border border-gray-300 px-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 flex-1 rounded-l-lg bg-gray-200`}
       value={value}
       onChange={(e) => onChange(e.target.value)}
       disabled={disabled}
